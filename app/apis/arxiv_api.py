faddf6ae22c4959a3cc4ec6ce2543f31db124f80
Pooria <pooria_taghdiri@hotmail.com>
2026-05-30 21:12:42 -0700

    Handle cleared max_results in arXiv adapter

--- app/apis/arxiv_api.py
+++ app/apis/arxiv_api.py
@@ -10,14 +10,18 @@ def get_arxiv_data(
     sortBy,
     sortOrder,
 ):
-    request = ArxivSearchRequest(
-        query=search_input,
-        search_field=search_field,
-        id_list=id_list or None,
-        start=start,
-        max_results=max_results,
-        sort_by=sortBy,
-        sort_order=sortOrder,
-    )
+    request_params = {
+        "query": search_input,
+        "search_field": search_field,
+        "id_list": id_list or None,
+        "start": start,
+        "sort_by": sortBy,
+        "sort_order": sortOrder,
+    }
+
+    if max_results is not None:
+        request_params["max_results"] = max_results
+
+    request = ArxivSearchRequest(**request_params)
 
     return ArxivClient().fetch_articles_xml(request)
